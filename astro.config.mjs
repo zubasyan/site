ec3fb392e89500633f50549a5f53a0ba6880feda
zubasyan <mikoraio914@icloud.com>
2026-01-11 22:15:21 +0900

    project rename site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({  
   site: 'https://zubasyan.github.io',  
-  base: '/blog',
+  base: '/site',
 });
