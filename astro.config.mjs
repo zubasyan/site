7274aff17b1aae47d5fb76fa1809b06d17df22cc
zubasyan <mikoraio914@icloud.com>
2026-01-13 23:39:25 +0900

    make katex usable and first blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
@@ -8,4 +11,9 @@ export default defineConfig({
   site: 'https://zubasyan.github.io',
   base: '/site',
   integrations: [preact()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
 });
---
+++ src/pages/blog/2026/01/gag.md
@@ -0,0 +1,16 @@
+---
+layout: ../../../../layouts/BlogLayout.astro
+title: "2026年一発目 渾身の機械学習ギャグ" 
+pubDate: 2026/01/13
+category: "茶番"
+tags: ["ギャグ"]
+---
+
+![ギャグ](images/父ちゃんReLUじゃん.png)
+
+参考までに  
+$f(x) = \max(0,x)$
+
+#### 参考文献
+[1]["正規化線形関数
+",wikipedia](https://ja.wikipedia.org/wiki/%E6%AD%A3%E8%A6%8F%E5%8C%96%E7%B7%9A%E5%BD%A2%E9%96%A2%E6%95%B0)
