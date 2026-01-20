2d4a3fbeecf4724f2d9feaf46de82e303d75eae3
zubasyan <mikoraio914@icloud.com>
2026-01-20 23:12:41 +0900

    ホームページを変更

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,7 +8,7 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}！ 訪問いただきありがとうございます！</h3>
+      <h3>{greeting}！</h3>
       <button onClick={() => setGreeting(randomMessage())}>
         挨拶を変える
       </button>
